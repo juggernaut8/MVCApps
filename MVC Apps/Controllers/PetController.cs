22a6b9df02b4049691c9afba331effb21f5d698f
juggernaut8 <fixersolver8888@gmail.com>
2013-11-14 02:30:38 -0600

    1st commit

---
+++ MVC Apps/Controllers/PetController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Apps.Controllers
+{
+    public class PetController : Controller
+    {
+        //
+        // GET: /Pet/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Display()
+        {
+            var name = (string)RouteData.Values["id"];
+          //  var model = PetManagement.GetByName[name];
+
+            //if (model == null)
+            //{
+            //    return RedirectToAction("NotFound");
+
+            //}
+            //return View(model);
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
+        public FileResult DownloadPetPicture()
+        {
+            var name = (string)RouteData.Values["id"];
+            var picture = "Content/Uploads/" + name + ".jpg";
+            var contentType = "image/jpg";
+            var fileName = name + ".jpg"; //removing this will load the picture in browser
+            return File(picture, contentType, fileName);
+        }
+
+        public HttpStatusCodeResult UnauthorizedError()
+        {
+            return new HttpUnauthorizedResult("Custom Unauthorized Error");
+        }
+
+    }
+}
---
+++ MVC Apps/Models/LoginModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Apps.Models
+{
+    public  class LoginModel
+    {
+        [Required(ErrorMessage = "This Username is required")]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "The Password is required")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "The Password is invalid")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Pass word")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+
+    }
+}
